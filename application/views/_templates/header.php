3ce4c27682f15698e4ac69cf529822d0f8d7183f
ratanparai <ratanparai@gmail.com>
2014-10-17 16:43:37 +0600

    Some CSS code writing

---
+++ application/views/_templates/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>IIUC hostel</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!--  CSS  -->
+    <link rel="stylesheet" href="<?php echo URL; ?>/public/css/style.css" />
+</head>
+<body>
+	<!-- logo and navigation stuff -->
+    <div class="row">
+    	<!-- logo -->
+    	<div class="small-12 medium-4 columns">
+        	<div class="panel">
+            	<p>LOGO</p>
+            </div>
+        </div>
+        <!-- navigation -->
+        <div class="small-12 medium-8 columns">
+        	<div class="panel">
+            	<p>navigation: <?php echo URL; ?></p>
+            </div>
+        </div>
+    </div>
--- application/views/index/index.php
+++ application/views/index/index.php
@@ -1,1 +1,14 @@
-You are here
+<div class="row">
+	<div class="small-12 medium-6 columns">
+    	<div class="panel">
+            <input type="text" name="username" placeholder="username" />
+            <input type="password" name="password" placeholder="password" />
+            <button>Log me in!</button>
+        </div>
+    </div>
+    <div class="small-12 medium-6 columns">
+    	<div class="panel">
+        	<p>Sidebar</p>
+        </div>
+    </div>
+</div>
